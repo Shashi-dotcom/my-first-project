918c2e42a7704b8cff197020205da0e24c874b33
Shashi-dotcom <shashikanthjames007@gmail.com>
2024-10-17 17:29:08 +0530

    Your commit message describing the changes

--- Server/index.js
+++ Server/index.js
@@ -2,8 +2,15 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 const  UserModel=require('./model/users')
+require('dotenv').config();
+// mongoose.connect('mongodb://127.0.0.1:27017/crud')
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/crud')
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const app= express()
 app.use(cors())
