0148c6b5c4aed226e756eb2ee9cda7c23fb7f856
Shashi-dotcom <shashikanthjames007@gmail.com>
2024-10-18 16:31:15 +0530

    updated vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Bind to 0.0.0.0 for external access
+  build: {
+    outDir:'dist'  // Bind to 0.0.0.0 for external access
   }
 })
