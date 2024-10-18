519b5377cfe446d431fb4a4de9a4fa7d49c94b43
Shashi-dotcom <shashikanthjames007@gmail.com>
2024-10-18 17:31:58 +0530

    updated cors with specific url

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,17 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
+const app= express()
 const  UserModel=require('./model/users')
 require('dotenv').config();
 // mongoose.connect('mongodb://127.0.0.1:27017/crud')
 // const mongoose = require('mongoose');
+
+app.use(cors({
+    origin: 'https://my-first-project-3-qae4.onrender.com',
+    methods : ['GET','POST','PUT','DELETE'],
+    credentials : true,
+}));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -12,8 +19,9 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-const app= express()
-app.use(cors())
+
+
+
 app.use(express.json())
 
 app.post("/createUser",(req,res)=>{
